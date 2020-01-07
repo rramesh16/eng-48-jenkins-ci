99956a044024b16c78fc24b78167d22064f59144
Rahavi Ramesh <RRamesh@spartaglobal.com>
2020-01-07 10:39:04 +0000

    README.md updated

--- environment/app/provision.sh
+++ environment/app/provision.sh
@@ -25,4 +25,48 @@ sudo rm /etc/nginx/sites-available/default
 sudo cp /home/ubuntu/environment/nginx.default /etc/nginx/sites-available/default
 
 # finally, restart the nginx service so the new config takes hold
-sudo service nginx restart
+sudo service nginx
+
+
+# Adding Java repository
+sudo add-apt-repository ppa:openjdk-r/ppa
+
+# Update and install openjdk
+sudo apt update
+sudo apt install openjdk-7-jdk
+
+# Installing Jenkins locally
+sudo wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt Update
+sudo apt install Jenkins
+
+#start Jenkins
+sudo systemctl start jenkins
+sudo nestat -plntu | grep 8080
+
+# install apache
+sudo apt install apache2
+sudo  a2enmod proxy
+sudo a2enmod proxy_http
+sudo $EDITOR /etc/apache2/sites-available/jenkins.conf
+<Virtualhost *:80>
+    ServerName        my.jenkins.id
+    ProxyRequests     Off
+    ProxyPreserveHost On
+    AllowEncodedSlashes NoDecode
+
+    <Proxy http://localhost:8080/*>
+      Order deny,allow
+      Allow from all
+    </Proxy>
+
+    ProxyPass         /  http://localhost:8080/ nocanon
+    ProxyPassReverse  /  http://localhost:8080/
+    ProxyPassReverse  /  http://my.jenkins.id/
+</Virtualhost>
+
+sudo a2ensite jenkins
+sudo systemctl restart apache2
+sudo systemctl restart jenkins
+sudo netstat -plntu | grep 80
